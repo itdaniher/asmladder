335c36d567ff22abf2e231c2ddc4d053108caa48
Ian Daniher <it.daniher@gmail.com>
2012-06-16 18:32:57 -0500

    more comments

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include "iotn44a.h"
 
+// overflow interrupt for TIM0
 ISR( TIM0_OVF_vect ) {
+	// toggle pin 1 of port A
 	 PORTA ^= 1 << PINA1;
+// exit interrupt
 }
 
 int main(void) {
-	// Set up port A pin 1 mode to output
+	// set up port A pin 1 mode to output
 	 DDRA = 1 << PINA1;
 	// prescale timer to 1/1024th the clock rate
 	TCCR0B |= (1<<CS02) | (1<<CS00);
@@ -13,6 +16,7 @@ int main(void) {
 	TIMSK0 |=1<<TOIE0;
 	// enable interrupts
 	__asm("sei");
+	// do nothing, forever
 	for(;;) {
 		__asm("nop");
 	}
