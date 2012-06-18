226f7a520f3b6b11f61e44668b4c50a9f7e837fb
Ian Daniher <it.daniher@gmail.com>
2012-06-18 14:43:09 -0500

    comment all the things

--- objdump2json.py
+++ objdump2json.py
@@ -30,10 +30,8 @@ def removeItems(regex, lst):
 		results.append(safeMatch(re.search(regex, item)))
 	return [item if item not in results else '' for item in lst]
 
-# remove includes and function definitions from the C code
+# remove function definitions from the C code
 filec = removeItems(matchInclude, filec)
-#filec = removeItems(matchFunctions, filec)
-
 
 last = 0
 functionList = []
@@ -43,37 +41,37 @@ for function in functions:
 	functionList.append([])
 	lineList = []
 
-	if len(function) != 0:
+	if len(function) != 0: # robustify the function parser by ignoring empty ones
 
-		for item in function:
+		for item in function: # iterate through the lists of lines
 
 			if item[0] == '/': # if not a /path:number reference, then assume it to be assembly
 
 				lineNumber = int(item.split(':')[-1]) # determine the line number mentioned
 				relevantLines = filter(bool, filec[last:lineNumber]) # all the non-empty lines between last and current line numbers
-				comments = [item.replace('//', '').strip() for item in relevantLines if item.find('//') != -1]
-				code = [item.strip() for item in relevantLines if item.find('//') == -1]
-				lineList.append({'comment': comments})
-				lineList[-1].update({'code': code})
-				lineList[-1].update({'asm': []})
-				last = lineNumber
+				comments = [item.replace('//', '').strip() for item in relevantLines if item.find('//') != -1] # parse source code lines for comments
+				code = [item.strip() for item in relevantLines if item.find('//') == -1] # clean lines
+				lineList.append({'comment': comments}) # make new dictionary in lineList, add comments
+				lineList[-1].update({'code': code}) # append the raw C code
+				lineList[-1].update({'asm': []}) # make an empty dictionary for the parsed ASM
+				last = lineNumber 
 
 			else:
 
 				item = item.split() # split the line
-				if item.count(';') > 0: 
-					item = item[0:item.index(';')]
-				try:
-					int(item[0])
-					functionList[-1].append(re.sub('[<>:]', '', item[1]))
+				if item.count(';') > 0: # if the line contains a semicolon
+					item = item[0:item.index(';')] # remove it, and everything after it
+				try: # check to see if the line starts with an offset
+					int(item[0]) # asm lines specifying function/offset start with an integer
+					functionList[-1].append(re.sub('[<>:]', '', item[1])) # use the string at index = 1 as the name of the function
 				except:
 
-					if item[0].find('()') == -1:
-						item = item[3::] # discard line number and bytecode
-						item = [item[0], list([item.strip(',') for item in item[1::]])]
-						lineList[-1]['asm'].append(item)
+					if item[0].find('()') == -1: # check to make sure that this isn't a function definition
+						item = item[3::] # discard line number and bytecode 
+						item = [item[0], list([item.strip(',') for item in item[1::]])] # kill extraneous commas
+						lineList[-1]['asm'].append(item) # append it to the asm object in the last line
 
-		functionList[-1].append(lineList)
+		functionList[-1].append(lineList) # append the line to the list of lines of the last function
 
 pprint.pprint(functionList)
-json.dumps(functionList, indent=1)
+open("main.json", "w").write(json.dumps(functionList, indent=1))
