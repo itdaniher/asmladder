661fd22c4e35729829816bbec072b87510c75890
Ian Daniher <it.daniher@gmail.com>
2012-06-18 22:21:23 -0500

    added demo code for xmega blinkylight

--- main.c
+++ main.c
@@ -1,19 +1,23 @@
-#include "iotn44a.h"
+#include "io.h"
 
-// overflow interrupt for TIM0
-ISR( TIM0_OVF_vect ) {
-	// toggle pin 1 of port A
-	 PORTA ^= 1 << PINA1;
+// overflow interrupt for TCC0
+ISR(TCC0_OVF_vect){ 
+// toggle pin 1 of port A
+	PORTA.OUTTGL = PIN1_bm;
 // exit interrupt
 }
 
 int main(void) {
 	// set up port A pin 1 mode to output
-	 DDRA = 1 << PINA1;
-	// prescale timer to 1/1024th the clock rate
-	TCCR0B |= (1<<CS02) | (1<<CS00);
-	// enable timer overflow interrupt
-	TIMSK0 |=1<<TOIE0;
+	PORTA.DIRSET = PIN1_bm;
+	// prescale timer to 1/8th the clock rate
+    TCC0.CTRLA = TC_CLKSEL_DIV8_gc;
+	// interrupt on timer overflow
+	TCC0.INTCTRLA = TC_OVFINTLVL_LO_gc;
+	// set timer period to full 16b
+	TCC0.PER = 0xFFFF;
+	// initalize timer count at 0
+    TCC0.CNT = 0;
 	// enable interrupts
 	__asm("sei");
 	// do nothing, forever
